c78d353bc5fa5a21d9fd064fcbd665fa5defd955
marve <codedmarve@gmail.com>
2022-06-18 22:04:37 +0100

    first commit

---
+++ ex07/ft_rev_int_tab.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_rev_int_tab.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: moduwole <moduwole@student.42wolfsburg.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/16 20:03:36 by coder             #+#    #+#             */
+/*   Updated: 2022/02/23 20:53:36 by moduwole         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+void	ft_rev_int_tab(int *tab, int size)
+{
+	int		start;
+	int		end;
+	int		temp;
+
+	end = size - 1;
+	start = 0;
+	while (start < end)
+	{
+		temp = tab[start];
+		tab[start] = tab[end];
+		tab[end] = temp;
+		start++;
+		end--;
+	}
+}
---
+++ ex08/ft_sort_int_tab.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_int_tab.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: moduwole <moduwole@student.42wolfsburg.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/16 20:44:44 by coder             #+#    #+#             */
+/*   Updated: 2022/02/24 00:15:54 by moduwole         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_sort_int_tab(int *tab, int size)
+{
+	int	temp;
+	int	i;
+	int	j;
+
+	i = 0;
+	while (i < size -1)
+	{
+		j = 0;
+		while (j < size - i - 1)
+		{
+			if (tab[j] > tab[j + 1])
+			{
+				temp = tab[j];
+				tab[j] = tab[j + 1];
+				tab[j + 1] = temp;
+			}
+			j++;
+		}
+		i++;
+	}
+}
+ 
